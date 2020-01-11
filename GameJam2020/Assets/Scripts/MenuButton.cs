383ebdb017b9dc32c14345218cbc93d63ec0410c
vaquierm <michaelvaquier74@gmail.com>
2020-01-11 17:47:10 -0500

    Merge

--- GameJam2020/Assets/Scripts/MenuButton.cs
+++ GameJam2020/Assets/Scripts/MenuButton.cs
@@ -1,26 +1,11 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 public class MenuButton : MonoBehaviour
 {
     public void OnClick()
     {
         Debug.Log("Play button pressed.");
-        StartCoroutine(ChangeLevel());
-    }
-
-    private static IEnumerator ChangeLevel()
-    {
-        yield return new WaitForSeconds(2);
-        if (SceneManager.GetActiveScene().name == "MainMenu")
-        {
-            SceneManager.LoadScene("MainScene");
-        }
-        else if (SceneManager.GetActiveScene().name == "MainScene")
-        {
-            SceneManager.LoadScene("MainMenu");
-        }
+        SceneHelper.NextScene();
     }
 
     public void Quit()
